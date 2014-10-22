d997d2c8ba46c96f46e134f439c18c69710d8c29
Daniele Guido <gui.daniele@gmail.com>
2014-10-22 11:13:50 +0200

    visualizing iframe for vimeo video

--- client/app/scripts/directives/vimeo.js
+++ client/app/scripts/directives/vimeo.js
@@ -13,13 +13,12 @@ angular.module('svenClientApp')
       restrict: 'E',
       scope:{
         videoid:'=',
-        height: '=',
-        width: '=',
-        title: '='
+        height: '='
       },
       link: function postLink(scope, element, attrs) {
-        element.html('' + scope.videoid.slug);
+        var src = 'http://player.vimeo.com/video/' + scope.videoid;
         console.log(scope)
+        element.html('<iframe src="'+src+'" frameborder="0" width="100%" height="'+scope.height+'" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
         //<iframe ng-src="{{'//player.vimeo.com/video/' + document.tags.oembed_video_id.name}}" ng-width="{{document.tags.oembed_width.name}}" ng-height="{{document.tags.oembed_height.name}}" frameborder="0" title="{{document.name}}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       }
     };
