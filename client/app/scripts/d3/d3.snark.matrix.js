3dab5c676beea5bf4e14a8e112563eb106971e04
Daniele Guido <gui.daniele@gmail.com>
2014-11-10 12:26:12 +0100

    working on concept visualisation

--- client/app/scripts/d3/d3.snark.matrix.js
+++ client/app/scripts/d3/d3.snark.matrix.js
@@ -86,8 +86,11 @@
             measure: 'tf'
           },
 
-          local_min = d3.min(_data, function(d) {return d[options.measure]}),
-          local_max = d3.max(_data,function(d) {return d[options.measure]}),
+          tf_min = d3.min(_data, function(d) {return d.tf}),
+          tf_max = d3.max(_data,function(d) {return d.tf}),
+
+          tfidf_min = d3.min(_data, function(d) {return d.tf_idf}),
+          tfidf_max = d3.max(_data,function(d) {return d.tf_idf}),
           
           elements = _svg
             .selectAll(".block")
@@ -95,8 +98,10 @@
       
       // cfr http://bost.ocks.org/mike/nest/
       matrix.draw(elements, {
-        min: local_min,
-        max: local_max,
+        tf_min: tf_min,
+        tf_max: tf_max,
+        tfidf_min: tfidf_min,
+        tfidf_max: tfidf_max,
         measure: options.measure
       });
     };
@@ -109,10 +114,13 @@
       
       var offsetx = 200,
 
-          size = d3.scale.log()
-            .base(Math.E)
-            .domain([options.min, options.max])
-            .range([2,18]),
+          tf_size = d3.scale.linear()
+            .domain([options.tf_min, options.tf_max])
+            .range([2,15]),
+
+          tfidf_size = d3.scale.linear()
+            .domain([options.tfidf_min, options.tfidf_max])
+            .range([2,15]),
 
         update_selection = elements,
         enter_selection = elements.enter()
@@ -120,9 +128,8 @@
           .attr('class', function(d) {
             return 'block ' + d.status;
           }),
-        switchers = enter_selection
-          .append('g')
-            .attr('class', 'switcher');
+        switchers;
+        
 
         // set basic transform for each row (y)
         enter_selection
@@ -134,6 +141,9 @@
         // ADDING ELEMENTS LEFT TO RIGHT
         //
         // append base rectangle
+        switchers = enter_selection
+          .append('g')
+            .attr('class', 'switcher');
         switchers
           .append('circle')
             .attr('class', 'toggler')
@@ -144,7 +154,6 @@
             .attr('cx', 10)
             .attr('cy', 0)
 
-        
 
         // write text labels (they won't change)
         enter_selection
@@ -154,8 +163,32 @@
               return d.cluster || '...'
             });
 
-        // adding switchers
-        
+        // adding tfrs
+        enter_selection
+          .append('circle')
+            .attr({
+              'class': 'tf',
+              cx: 160,
+              opacity: ".4",
+              r: function(d) {
+                return tf_size(+d.tf);
+              },
+              fill: "gold"
+            })
+
+        enter_selection
+          .append('circle')
+            .attr({
+              'class': 'tf_idf',
+              cx: 160,
+              opacity: ".4",
+              r: function(d) {
+                console.log(d);
+                return tfidf_size(+d.tf_idf);
+              },
+              fill: "gold"
+            })
+
 
         
 
@@ -183,7 +216,7 @@
               .append('circle')
               .attr('class', 'measure')
               .attr('r', function(d){
-                return d[options.measure]? Math.max(1, size(d[options.measure])):1;
+                return 2//return d[options.measure]? Math.max(1, size(d[options.measure])):1;
               })
               .attr('cx', function(d,i) {
                 return i*7 + offsetx;
@@ -210,102 +243,8 @@
           .exit()
           .remove()
           
-
-      return;
-
-
-        
-      // update row elements
-
-      update_selection
-        .selectAll(".block")
-        .attr("y", function(d,i) {
-          return i*26 + 26*2;
-        });
-
-      enter_selection
-        .append("text")
-        .text(function(d) {
-          return d.content || '...'
-        })
-        .attr("y", function(d,i) {
-          return i*26 + 26*2;
-        })
-        .style("fill", "#333333");
-      
-
-      update_selection
-        .selectAll("circle.global")
-        .transition()
-          .duration(750)
-          .attr("r", function(d){ return Math.max(1, size(d[options.measure]))})
-          .attr("cx", 400)
-          .attr("cy", function(d, i) {
-            console.log('changing', i)
-            return i*26 + 26*2;
-          })
-          //.style("fill", function(d){ return d[options.measure]==options.max?"rgb(25,158,154)":"#333333"})
-      
-
-
-      // draw circle (globals)
-      enter_selection
-        .append("circle")
-        .attr("class", "global")
-        .attr("fill-opacity", .3)
-        .attr("r", function(d){return Math.max(1, size(d[options.measure]))})
-        .attr("class", "circle")
-        .attr("cx", 400)
-        .attr("cy", function(d, i) {
-          return i*26 + 26*2;
-        })
-
-      update_selection
-        .exit()
-        .remove()
-
-
-      // for each actor. calculate actor max et actor min
-      for(var i=0; i< Math.min(_headers.length, 50); i++) {
-        var column_id = _headers[i].id,
-            radius = function(d){ 
-          var match = d.tags.filter(function(j){
-            return j.id == column_id
-          });
-          try{
-            //if(match.length)
-              //console.log(match, options.measure , match[0][options.measure])
-            return match.length? Math.max(2, size(match[0][options.measure])): 1
-          } catch(err) {
-            console.log(d, 'has error', err)
-            return 1;
-          }
-        };
-        
-        /*update_selection
-          .selectAll("circle.tag")
-          .transition()
-          .duration(750)
-          .attr("r", radius)
-          */
-        
-        enter_selection
-          .append("circle")
-          .attr("class", "tag  "+ column_id)
-          //.style("fill", "#333333")
-          .attr("fill-opacity", .4)
-          .attr("r", radius)
-          
-          .attr("cx", 400 + (i+1)*20)
-          .attr("cy", function(d, j) {
-            return j*26 + 26*2;
-          })
-        //_data[0].tags[i][options.measure]
-        
-      }
-      
-
-    };
+        return matrix;
+      };
 
     return matrix;
 
