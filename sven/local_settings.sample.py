2625631982bf559cb70117c4dc8833c226c68979
Daniele Guido <gui.daniele@gmail.com>
2014-11-15 14:22:29 +0100

    attaching detaching tag

--- sven/local_settings.sample.py
+++ sven/local_settings.sample.py
@@ -23,6 +23,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_URL = '/login/'
 
 PYTHON_INTERPRETER = '/home/daniele/.virtualenvs/sven/bin/python' # mine, given as exemple. Cfr virtualenv doc.
 
