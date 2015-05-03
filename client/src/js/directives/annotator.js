1e16cbff11ce8805ae76708b688ba65270452ff2
Daniele Guido <gui.daniele@gmail.com>
2015-05-03 20:36:42 +0200

    adding annotations

---
+++ client/src/js/directives/annotator.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name svenClientApp.directive:annotator
+ * @description
+ * # concepts
+ */
+angular.module('sven')
+  .directive('annotator', function($compile) {
+    return {
+      restrict : 'A',
+      scope:{
+        content: '='
+      },
+      link : function(scope, element, attrs) {
+        element.html(scope.content)
+      }
+    }
+  })
